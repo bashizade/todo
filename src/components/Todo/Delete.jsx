b1ea66904b8825c9252270897a72df9025a76423
bashizade.dev <bashizade.official@gmail.com>
2023-09-23 12:31:01 +0330

    fix delete

--- src/components/Todo/Delete.jsx
+++ src/components/Todo/Delete.jsx
@@ -1,14 +1,30 @@
 import { Axios } from "../../configs/Axios";
 import Toastify from "../Toastify";
+import Swal from "sweetalert2";
 
 const Delete = (taskId, getData) => {
-    Axios.delete('tasks/' + taskId).then(res => {
-        Toastify("success", "با موفقیت حذف شد");
-        getData()
-    }).catch(err => {
-        console.log(err);
-        Toastify("error", "خطا در ارسال درخواست")
-    })
-}
+  Swal.fire({
+    title: "آیا مطمئن به حذف تسک هستید ؟",
+    showCancelButton: true,
+    confirmButtonText: "حذف",
+    cancelButtonText: "خیر",
+    cancelButtonColor: 'red'
+  }).then((result) => {
+    /* Read more about isConfirmed, isDenied below */
+    if (result.isConfirmed) {
+      Axios.delete("tasks/" + taskId)
+        .then((res) => {
+          Toastify("success", "با موفقیت حذف شد");
+          getData();
+        })
+        .catch((err) => {
+          console.log(err);
+          Toastify("error", "خطا در ارسال درخواست");
+        });
+    } else if (result.isDenied) {
+        Toastify("error", "حذف لغو شد");
+    }
+  });
+};
 
 export default Delete;
